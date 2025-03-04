bcf4a6f6b866e496a685fc49dc9e31933ae6dd6e
ttrnkocy <163468942+ttrnkocy@users.noreply.github.com>
2025-03-04 03:10:44 +0100

    Update sensors.cpp

--- components/balboa_spa/sensor/sensors.cpp
+++ components/balboa_spa/sensor/sensors.cpp
@@ -40,7 +40,8 @@ void BalboaSpaSensors::update(SpaState* spaState) {
             }
             break;
         default:
-            ESP_LOGD("Spa/Sensors/UnknownSensorType", String((uint8_t)sensor_type, 0).c_str());
+            //ESP_LOGD("Spa/Sensors/UnknownSensorType", String((uint8_t)sensor_type, 0).c_str());
+            ESP_LOGD("Spa/Sensors/UnknownSensorType", std::to_string((uint8_t)sensor_type).c_str());
             // Unknown enum value. Ignore
             return;
     }
